cac3b497b057a76b2d8ee282821eded123a05ddc
Baron Lip <baronlip@gmail.com>
2019-01-31 12:16:21 -0600

    Commit to end Conflict.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,25 @@
-var overlayOne = document.querySelector('.challenge .active');
+var statements = document.querySelectorAll(".statements");
+var missionStatement = document.querySelector(".mission_statement");
+var challengeBtn = document.querySelector(".challenge-btn");
+var challengeOverlay = document.querySelector(".challenge_statement");
 
-overlayOne.addEventListener('mouseover', function(e){
-  if(e.target.classList.contains('challenge active')) {
-    e.target.classList.remove('active');
-  } else {
-    e.target.classList.add('active');
+challengeBtn.addEventListener("mouseenter", function(e) {
+  // remove active class from all the statements
+  for (let i = 0; i < statements.length; i++) {
+    statements[i].classList.remove("active");
   }
+  // add active class to this specific statement
+  challengeOverlay.classList.add("active");
 });
 
-
+challengeBtn.addEventListener("mouseleave", function(e) {
+  // remove active class from all the statements
+  for (let i = 0; i < statements.length; i++) {
+    statements[i].classList.remove("active");
+  }
+  // add active class to this specific statement
+  missionStatement.classList.add("active");
+});
 // 1.  mouse over - mission statement text fades out.
 // 2. challenge statement fades in.
-// 3. mouse off - challenge statement fades out. 
+// 3. mouse off - challenge statement fades out.
