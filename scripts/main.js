2e7d740fce1979b7f6b75dad463438b4415d2cd4
Baron Lip <baronlip@gmail.com>
2019-02-01 16:49:46 -0600

    Applied JavaScript to each button

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 var statements = document.querySelectorAll(".statements");
 var missionStatement = document.querySelector(".mission_statement");
+
 var challengeBtn = document.querySelector(".challenge-btn");
 var challengeOverlay = document.querySelector(".challenge_statement");
 
@@ -20,6 +21,75 @@ challengeBtn.addEventListener("mouseleave", function(e) {
   // add active class to this specific statement
   missionStatement.classList.add("active");
 });
-// 1.  mouse over - mission statement text fades out.
-// 2. challenge statement fades in.
-// 3. mouse off - challenge statement fades out.
+
+
+
+// Events button JS.-----
+var eventsBtn = document.querySelector(".events-btn");
+var eventsOverlay = document.querySelector(".events_statement");
+
+eventsBtn.addEventListener("mouseenter", function(e) {
+  // remove active class from all the statements
+  for (let i = 0; i < statements.length; i++) {
+    statements[i].classList.remove("active");
+  }
+  // add active class to this specific statement
+  eventsOverlay.classList.add("active");
+});
+
+eventsBtn.addEventListener("mouseleave", function(e) {
+  // remove active class from all the statements
+  for (let i = 0; i < statements.length; i++) {
+    statements[i].classList.remove("active");
+  }
+  // add active class to this specific statement
+  missionStatement.classList.add("active");
+});
+
+
+
+// Clubs button JS.-----
+var clubsBtn = document.querySelector(".clubs-btn");
+var clubsOverlay = document.querySelector(".clubs_statement");
+
+clubsBtn.addEventListener("mouseenter", function(e) {
+  // remove active class from all the statements
+  for (let i = 0; i < statements.length; i++) {
+    statements[i].classList.remove("active");
+  }
+  // add active class to this specific statement
+  clubsOverlay.classList.add("active");
+});
+
+clubsBtn.addEventListener("mouseleave", function(e) {
+  // remove active class from all the statements
+  for (let i = 0; i < statements.length; i++) {
+    statements[i].classList.remove("active");
+  }
+  // add active class to this specific statement
+  missionStatement.classList.add("active");
+});
+
+
+
+// Forum button JS-----
+var forumBtn = document.querySelector(".forum-btn");
+var forumOverlay = document.querySelector(".forum_statement");
+
+forumBtn.addEventListener("mouseenter", function(e) {
+  // remove active class from all the statements
+  for (let i = 0; i < statements.length; i++) {
+    statements[i].classList.remove("active");
+  }
+  // add active class to this specific statement
+  forumOverlay.classList.add("active");
+});
+
+forumBtn.addEventListener("mouseleave", function(e) {
+  // remove active class from all the statements
+  for (let i = 0; i < statements.length; i++) {
+    statements[i].classList.remove("active");
+  }
+  // add active class to this specific statement
+  missionStatement.classList.add("active");
+});
